a5b541b0dcb37795c5a68b2567155438e3f18418
mayankkr96 <mayankkr96@gmail.com>
2017-02-22 18:46:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,48 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Article = {
+  title: 'Makerafts | Article One',
+  heading: 'First Makeraft Article',
+  content: `<p>
+                This is content Part of the Article One.
+           </p>
+           `
+};
+
+
+function createtemplate (data) {
+    var titile = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var articletemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="box">
+                <div>
+                    <a href="/" style="color: white">Home</a>
+                </div>
+                <br> <hr/>
+                <h2>
+                    ${heading}
+                </h2>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return articletemplate;
+}
+    
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +60,7 @@ app.get('/ui/A-LRF.png', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(Article));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
