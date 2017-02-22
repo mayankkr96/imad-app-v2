254ac57ee07d10cce4c39215ef1e7e0c0026533c
mayankkr96 <mayankkr96@gmail.com>
2017-02-22 18:19:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/A-LRF.png', function (req, res) {
-  res.sendFile(path.join('madi.png'));
+app.get('/ui/A-LRF.png', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui', 'A-LRF.png'));
 });
 
 app.get('/article-one', function(req, res){
