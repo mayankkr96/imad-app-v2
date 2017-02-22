8b2c4e1c33b621dfff3fe1f89d5d77365d268cdb
mayankkr96 <mayankkr96@gmail.com>
2017-02-22 20:13:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,2 @@
 console.log('Loaded!');
-document.getElementById("maintext").innerHTML='Hello Makerafts';
-document.getElementById("mainimg").src='http://www.auntannie.com/Puppets/ThreeFinger/3FingerSheep170.jpg';
+document.getElementById("maintext").innerHTML='Welcome';
