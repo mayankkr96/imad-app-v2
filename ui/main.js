3da54e4a988662eedad5c2d80098df0fd3d14fed
mayankkr96 <mayankkr96@gmail.com>
2017-02-22 20:26:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
 document.getElementById("maintext").innerHTML='Welcome';
-var i
+var img=document.getElemntById("mainimg");
+var marginleft=0;
+function move()
+{
+    marginleft=marginleft+2;
+    img.style.marginLeft=marginleft + 'px';
+}
+img.onclick = function(){
+ var interval=setInterval(move , 20);   
+};
