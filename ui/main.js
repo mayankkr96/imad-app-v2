8fa2d1e85f56c5d25b494ea2606618ee835e9477
mayankkr96 <mayankkr96@gmail.com>
2017-02-26 17:52:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick=function(){
             }
         }
     };
-    request.open('GET','http://mayankkr96.imad.hasura-app.io',true);
+    request.open('GET','http://mayankkr96.imad.hasura-app.io/counter',true);
     request.send(null);
    
 };
