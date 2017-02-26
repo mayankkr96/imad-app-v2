8a17598120b759b6744bc92244541f6616469b44
mayankkr96 <mayankkr96@gmail.com>
2017-02-26 18:02:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,12 +15,12 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
     var request = new XMLHttpRequest();
-    request.onreadystatechange=function(){
+    request.onreadystatechange = function(){
         if (request.readyState===XMLHttpRequest.DONE){
             if (request.status===200){
                 var counter=request.responseText;
                 var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                span.innerHTML=counter;
             }
         }
     };
